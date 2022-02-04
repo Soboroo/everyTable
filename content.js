c5c001093b81759b4d3c304d766064c1dd363e2e
Soboroo <woo036325@gmail.com>
2022-02-04 16:47:52 +0900

    createButton function

--- content.js
+++ content.js
@@ -1,29 +1,35 @@
 const ol = document.querySelector("#container > aside > div.title > ol");
+ol.appendChild(
+  createButton("iCal", "tableExport", "light image setting", () => {
+    // get id of selected table
+    tableId = document
+      .querySelector("#container > aside > div.menu > ol > li.active > a")
+      .href.split("/")
+      .reverse()[0];
+    console.log(tableId);
+    let requestURL =
+      "https://api.everytime.kr/find/timetable/table?id=" + tableId;
+    let request = new XMLHttpRequest();
+    request.open("GET", requestURL);
+    request.responseType = "text";
+    request.withCredentials = true;
+    request.onload = () => {
+      let response = request.response;
+      console.log(response);
+    };
+    request.send();
+  })
+);
 
-const newButton = document.createElement("li");
-newButton.setAttribute("data-modal", "tableExport");
-newButton.addEventListener("click", () => {
-  tableId = document
-    .querySelector("#container > aside > div.menu > ol > li.active > a")
-    .href.split("/")
-    .reverse()[0];
-  console.log(tableId);
-  let requestURL =
-    "https://api.everytime.kr/find/timetable/table?id=" + tableId;
-  let request = new XMLHttpRequest();
-  request.open("GET", requestURL);
-  request.responseType = "text";
-  request.withCredentials = true;
-  request.onload = () => {
-    let response = request.response;
-    console.log(response);
-  };
-  request.send();
-});
+function createButton(text, liModal, aClass, handler) {
+  const newButton = document.createElement("li");
+  newButton.setAttribute("data-modal", liModal);
+  newButton.addEventListener("click", handler);
 
-const a = document.createElement("a");
-a.setAttribute("class", "light image setting");
-a.innerText = "iCal";
+  const a = document.createElement("a");
+  a.setAttribute("class", aClass);
+  a.innerText = text;
 
-newButton.appendChild(a);
-ol.appendChild(newButton);
+  newButton.appendChild(a);
+  return newButton;
+}
