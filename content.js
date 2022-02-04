2258e5a629c9e0002264e8933f8aad6e42f4a69f
Soboroo <woo036325@gmail.com>
2022-02-04 18:37:41 +0900

    custom form

--- content.js
+++ content.js
@@ -1,66 +1,92 @@
-createForm("iCal", "tableCustom", "Export").appendTo("#container");
-$("tableCustom").removeAttr("style");
+$().ready(function () {
+  // common.js
+  //----------------------
+  $(".modal")
+    .bind("beforeShow", function (event) {
+      if (event.target !== this) return false;
+      var $modal = $(this);
+      var $modalwrap = $("<div></div>").addClass("modalwrap");
+      if ($modal.hasClass("popup")) $modalwrap.addClass("lighter");
+      $modalwrap.insertBefore($modal);
+      $modal.css({
+        "margin-left": -($modal.outerWidth() / 2),
+        "margin-top": -($modal.outerHeight() / 2),
+      });
+    })
+    .bind("afterHide", function (event) {
+      if (event.target !== this) return false;
+      var $modal = $(this);
+      $("div.modalwrap").remove();
+    });
+  $(document).on("click", "div.modalwrap", function (event) {
+    $(".modal:visible").hide();
+  });
+  //----------------------
 
-createButton("iCal", "tableCustom", "light image setting").appendTo(
-  "#container > aside > div.title > ol"
-);
-$('[data-modal="tableCustom"]').on("click", () => {
-  $("#tableCustom").show();
-});
-$("a.close", "#tableCustom").on("click", function () {
-  $("#tableCustom").hide();
-});
+  createForm("iCal", "tableCustom", "Export").appendTo("#container");
+  $("tableCustom").removeAttr("style");
 
-function createButton(text, liModal, aClass, handler) {
-  const $newButton = $("<li>").attr("data-modal", liModal);
-  $newButton.click(handler);
-
-  const $a = $("<a>").addClass(aClass).text(text);
-
-  $newButton.append($a);
-  return $newButton;
-}
-
-function createForm(text, id, submitButtonText = null, handler = null) {
-  const $form = $("<form>").attr("id", id).addClass("modal");
-  const $header = $("<h3>").text(text);
-
-  const $closeButton = $("<a>").addClass("close");
-  $closeButton.click(() => {
-    $("#" + id).hide();
+  createButton("iCal", "tableCustom", "light image setting").appendTo(
+    "#container > aside > div.title > ol"
+  );
+  $('[data-modal="tableCustom"]').on("click", () => {
+    $("#tableCustom").show();
+  });
+  $("a.close", "#tableCustom").on("click", function () {
+    $("#tableCustom").hide();
   });
 
-  $form.append($closeButton);
-  $form.append($header);
+  function createButton(text, liModal, aClass, handler) {
+    const $newButton = $("<li>").attr("data-modal", liModal);
+    $newButton.click(handler);
 
-  if (submitButtonText !== null) {
-    const $submitButton = $("<input>")
-      .attr("type", "submit")
-      .val(submitButtonText)
-      .addClass("button");
-    $submitButton.click(handler);
-    $form.append($submitButton);
+    const $a = $("<a>").addClass(aClass).text(text);
+
+    $newButton.append($a);
+    return $newButton;
   }
 
-  return $form;
-}
+  function createForm(text, id, submitButtonText = null, handler = null) {
+    const $form = $("<form>").attr("id", id).addClass("modal");
+    const $header = $("<h3>").text(text);
 
-function test() {
-  // get id of selected table
-  tableId = document
-    .querySelector("#container > aside > div.menu > ol > li.active > a")
-    .href.split("/")
-    .reverse()[0];
-  console.log(tableId);
-  let requestURL =
-    "https://api.everytime.kr/find/timetable/table?id=" + tableId;
-  let request = new XMLHttpRequest();
-  request.open("GET", requestURL);
-  request.responseType = "text";
-  request.withCredentials = true;
-  request.onload = () => {
-    let response = request.response;
-    console.log(response);
-  };
-  request.send();
-}
+    const $closeButton = $("<a>").addClass("close");
+    $closeButton.click(() => {
+      $("#" + id).hide();
+    });
+
+    $form.append($closeButton);
+    $form.append($header);
+
+    if (submitButtonText !== null) {
+      const $submitButton = $("<input>")
+        .attr("type", "submit")
+        .val(submitButtonText)
+        .addClass("button");
+      $submitButton.click(handler);
+      $form.append($submitButton);
+    }
+
+    return $form;
+  }
+
+  function test() {
+    // get id of selected table
+    tableId = document
+      .querySelector("#container > aside > div.menu > ol > li.active > a")
+      .href.split("/")
+      .reverse()[0];
+    console.log(tableId);
+    let requestURL =
+      "https://api.everytime.kr/find/timetable/table?id=" + tableId;
+    let request = new XMLHttpRequest();
+    request.open("GET", requestURL);
+    request.responseType = "text";
+    request.withCredentials = true;
+    request.onload = () => {
+      let response = request.response;
+      console.log(response);
+    };
+    request.send();
+  }
+});
