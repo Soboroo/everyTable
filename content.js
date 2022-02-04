a57fb664d8eff8bbc4fb56cf47f402d9d0428cb6
Soboroo <woo036325@gmail.com>
2022-02-04 14:13:47 +0900

    Create button

--- content.js
+++ content.js
@@ -1,10 +1,29 @@
-console.log("content.js loaded");
-console.log(document);
+const ol = document.querySelector("#container > aside > div.title > ol");
 
-// html에 script.js 추가
-var s = document.createElement("script");
-s.src = chrome.runtime.getURL("script.js");
-s.onload = function () {
-  this.remove();
-};
-(document.head || document.documentElement).appendChild(s);
+const newButton = document.createElement("li");
+newButton.setAttribute("data-modal", "tableExport");
+newButton.addEventListener("click", () => {
+  tableId = document
+    .querySelector("#container > aside > div.menu > ol > li.active > a")
+    .href.split("/")
+    .reverse()[0];
+  console.log(tableId);
+  let requestURL =
+    "https://api.everytime.kr/find/timetable/table?id=" + tableId;
+  let request = new XMLHttpRequest();
+  request.open("GET", requestURL);
+  request.responseType = "text";
+  request.withCredentials = true;
+  request.onload = () => {
+    let response = request.response;
+    console.log(response);
+  };
+  request.send();
+});
+
+const a = document.createElement("a");
+a.setAttribute("class", "light image setting");
+a.innerText = "iCal";
+
+newButton.appendChild(a);
+ol.appendChild(newButton);
