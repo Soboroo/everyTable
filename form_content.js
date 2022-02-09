256c23012b41e32433bc4ec4be76b8666dfde40f
Soboroo <woo036325@gmail.com>
2022-02-10 04:30:05 +0900

    .

--- form_content.js
+++ form_content.js
@@ -1,5 +1,16 @@
 $().ready(function () {
-  function getTableXML() {
+  const button = $("<a>Download</a>");
+  button.click(function () {
+    var table;
+    getTableXML(function (xml) {
+      table = xml;
+    });
+    const url = createiCalURL(table);
+    console.log(url);
+  });
+  $("#tableCustom").children("p").append(button);
+
+  function getTableXML(callback) {
     const tableId = document
       .querySelector("#container > aside > div.menu > ol > li.active > a")
       .href.split("/")
@@ -8,19 +19,26 @@ $().ready(function () {
     let requestURL =
       "https://api.everytime.kr/find/timetable/table?id=" + tableId;
     let request = new XMLHttpRequest();
-    request.open("GET", requestURL);
+    request.open("GET", requestURL, false);
     request.withCredentials = true;
     let response, table;
     request.onload = () => {
       response = request.responseXML;
       table = response.querySelector("table");
+      if (callback) {
+        callback(table);
+      }
     };
     request.send();
-    return table;
   }
 
   function getSemesterInfo(year, semester) {
-    const list = getSemesterListXML().querySelector("response").children;
+    //const list = getSemesterListXML().querySelector("response").children;
+    var list;
+    getSemesterListXML(function (xml) {
+      console.log(xml);
+      list = xml.querySelector("response").children;
+    });
     const ret = {};
     for (var i = 0; i < list.length; i++) {
       if (
@@ -35,21 +53,15 @@ $().ready(function () {
     return ret;
   }
 
-  function getSemesterListXML() {
-    let requestURL =
-      "https://api.everytime.kr/find/timetable/subject/semester/list";
-    let request = new XMLHttpRequest();
-    request.open("GET", requestURL);
-    request.withCredentials = true;
-    let response;
-    request.onload = () => {
-      response = request.responseXML;
-    };
-    request.send();
-    return response;
-  }
-
-  function createEvent(title, start, end, dayIndex, semesterInfo) {
+  function createEvent(
+    title,
+    start,
+    end,
+    dayIndex,
+    semesterInfo,
+    location = null,
+    description = null
+  ) {
     const day = ["MO", "TU", "WE", "TH", "FR", "SA", "SU"];
     var ret = "BEGIN:VEVENT\n";
     ret += "SUMMARY:" + title + "\n";
@@ -62,6 +74,12 @@ $().ready(function () {
       semesterInfo.end_date +
       "T235959\n";
     ret += "UID:" + getUUID() + "\n";
+    if (location !== null) {
+      ret += "LOCATION:" + location + "\n";
+    }
+    if (description !== null) {
+      ret += "DESCRIPTION:" + description + "\n";
+    }
     ret += "END:VEVENT\n";
     return ret;
   }
@@ -70,8 +88,30 @@ $().ready(function () {
       "BEGIN:VCALENDAR\n" +
       "VERSION:2.0\n" +
       "PRODID:-//everyTable//everytime timetable maker//KO\n";
+    const year = table.getAttribute("year");
+    const semester = table.getAttribute("semester");
+    const semesterInfo = getSemesterInfo(year, semester);
 
-    var blob = new Blob([buf], { type: "text/calendar" });
+    const events = table.children;
+    for (var i = 0; i < events.length; i++) {
+      const event = events[i];
+      const title = event.querySelector("name").getAttribute("value");
+      const times = event.querySelector("time").children;
+      for (var j = 0; j < times.length; j++) {
+        const time = times[j];
+        const day = time.getAttribute("day");
+        const start = timecodeToString(time.getAttribute("starttime"));
+        const end = timecodeToString(time.getAttribute("endtime"));
+        const location = time.getAttribute("place");
+
+        icalText += createEvent(title, start, end, day, semesterInfo, location);
+      }
+    }
+    icalText += "END:VCALENDAR";
+
+    var blob = new Blob([stringToArrayBuffer(icalText)], {
+      type: "text/calendar",
+    });
     var url = URL.createObjectURL(blob);
     return url;
   }
@@ -83,6 +123,35 @@ $().ready(function () {
       bufView[i] = text.charCodeAt(i);
     }
     return buf;
+  }
+
+  function getSemesterListXML(callback) {
+    let requestURL =
+      "https://api.everytime.kr/find/timetable/subject/semester/list";
+    let request = new XMLHttpRequest();
+    request.open("GET", requestURL);
+    request.withCredentials = true;
+    let response;
+    request.onload = () => {
+      response = request.responseXML;
+      console.log(response);
+      if (callback) {
+        callback(response);
+      }
+    };
+    request.send();
+  }
+  function timecodeToString(timecode) {
+    const hour = (timecode / 12).toString();
+    const minute = ((timecode % 12) * 5).toString();
+
+    if (hour.length == 1) {
+      hour = "0" + hour;
+    }
+    if (minute.length == 1) {
+      minute = "0" + minute;
+    }
+    return hour + minute;
   }
 
   function getUUID() {
@@ -97,4 +166,17 @@ $().ready(function () {
     );
   }
   // 출처: https://goni9071.tistory.com/209 [고니의꿈]
+
+  function ajax(url) {
+    return new Promise(function (resolve, reject) {
+      var xhr = new XMLHttpRequest();
+      xhr.onload = function () {
+        resolve(this.responseText);
+      };
+      xhr.onerror = reject;
+      xhr.open("GET", url);
+      xhr.withCredentials = true;
+      xhr.send();
+    });
+  }
 });
