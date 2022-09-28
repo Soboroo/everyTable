00f9d59eb8bcbb6ce848975acc125ca74f5524e6
Soboroo <woo036325@gmail.com>
2022-09-28 16:10:09 +0900

    formatting

--- content.js
+++ content.js
@@ -1,19 +1,10 @@
 $().ready(function () {
   $.get(chrome.runtime.getURL('/icalUI.html'), function (data) {
-    $($.parseHTML(data)).appendTo('#container');
-
-    var $modal = $("#tableCustom");
-    $modal.css({
-      "margin-left": -($modal.outerWidth() / 2),
-      "margin-top": -($modal.outerHeight() / 2),
-      "display": "none"
-    });
-    $("a.close", "#tableCustom").on("click", function () {
-      $("div.modalwrap").remove();
-      $("#tableCustom").hide();
-    });
+    htmlInject(data);
+    formatterInit($("#subjectFormatters"), $("#icalTitle"));
+    formatterInit($("#descriptionFormatters"), $("#icalDescription"));
+    moveElementToCenter($("#tableCustom"));
   });
-
 
   createButton("iCal", "tableCustom", "light image export").appendTo(
     "#container > aside > div.title > ol"
@@ -30,26 +21,41 @@ $().ready(function () {
     $modalwrap.insertBefore($("#tableCustom"));
     $("#tableCustom").show();
   });
-
-
-
-  function createButton(text, liModal, aClass, handler) {
-    const $newButton = $("<li>").attr("data-modal", liModal);
-    $newButton.click(handler);
-
-    const $a = $("<a>").addClass(aClass).text(text);
-
-    $newButton.append($a);
-    return $newButton;
-  }
-
-  function downloadIcal(ical) {
-    const url = ical.createIcalURL();
-    const a = document.createElement("a");
-    a.href = url;
-    a.download = ical.table.year + "년 " + ical.table.semester + "학기_" + ical.table.name + ".ics";
-    a.click();
-    a.remove();
-    window.URL.revokeObjectURL(url);
-  }
 });
+
+function moveElementToCenter($element) {
+  $element.css({
+    "margin-left": -($element.outerWidth() / 2),
+    "margin-top": -($element.outerHeight() / 2),
+    "display": "none"
+  });
+}
+
+function createButton(text, liModal, aClass, handler) {
+  const $newButton = $("<li>").attr("data-modal", liModal);
+  $newButton.click(handler);
+
+  const $a = $("<a>").addClass(aClass).text(text);
+
+  $newButton.append($a);
+  return $newButton;
+}
+
+function downloadIcal(ical) {
+  const url = ical.createIcalURL();
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = ical.table.year + "년 " + ical.table.semester + "학기_" + ical.table.name + ".ics";
+  a.click();
+  a.remove();
+  window.URL.revokeObjectURL(url);
+}
+function htmlInject(data) {
+  $($.parseHTML(data)).appendTo('#container');
+
+  $("a.close", "#tableCustom").on("click", function () {
+    $("div.modalwrap").remove();
+    $("#tableCustom").hide();
+  });
+}
+
--- icalUI.js
+++ icalUI.js
@@ -1,5 +1,9 @@
-$().ready(function () {
-	$("#titleFormat").on("click", function () {
-		$("#icalTitle").val($("#icalTitle").val() + "%s");
-	});
-});
+function formatterInit($formatters, $textarea) {
+	var formattingLatters = ["\\t", "\\p", "\\w", "\\c", "\\l", "\\i"];
+	for (let i = 0; i < $formatters.children().length; i++) {
+		$formatters.children().eq(i).on("click", () => {
+			$textarea.val($textarea.val() + formattingLatters[i]);
+			$textarea.focus();
+		});
+	}
+}
